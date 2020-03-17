7fb4e4afb6495b9f6860e3d5a68465cd284f1575
Rafael Altagnam <41834105+altagnam@users.noreply.github.com>
2020-03-16 23:09:17 -0300

    merge

---
+++ security-server/src/main/java/br/mg/puc/sica/security/server/SecurityServerApplication.java
@@ -0,0 +1,19 @@
+package br.mg.puc.sica.security.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe princpal do projeto - responsável por sua inicilização.
+ * @author rafael.altagnam
+ *
+ */
+@SpringBootApplication
+public class SecurityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityServerApplication.class, args);
+	}
+
+
+}
